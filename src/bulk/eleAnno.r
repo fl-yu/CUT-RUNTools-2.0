17685f12466422e14b666221e978877249dc0840
LonganNo1 <yufulong421@gmail.com>
2021-01-24 21:30:07 -0500

    peak ana

--- src/bulk/eleAnno.r
+++ src/bulk/eleAnno.r
@@ -1,14 +1,25 @@
 
+#!/n/app/R/3.3.3/bin/Rscript
+peak_overlap=commandArgs(trailingOnly = T)[1]
+peak_count=commandArgs(trailingOnly = T)[2]
+outputdir=commandArgs(trailingOnly = T)[3]
 
+peak_overlap <- read.table(peak_overlap, header=F)[, 1]
+peak_count <- read.table(peak_count, header=F)[, 1]
 
+peak_per <- round(peak_overlap*100/peak_count, 2)
+ele_name <- factor(c("UTR-5", "Promoter", "Exon", "Intron", "UTR-3", "Intragenic", "Intergenic"), levels=rev(c("UTR-5", "Promoter", "Exon", "Intron", "UTR-3", "Intragenic", "Intergenic")))
+setwd(outputdir)
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
+library(ggplot2)
+df <- data.frame(peak_per, ele_name)
 
-BiocManager::install("ChIPpeakAnno")
-library(ChIPpeakAnno)
-library(GenomicFeatures)
-library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+peak_overlap_per <- ggplot(data=df, aes(x=peak_per, y=ele_name)) +
+                    geom_bar(stat="identity", fill="steelblue")+
+                    theme_minimal() + ylab("genomic elements") + xlab("percentage")
 
+pdf("Overlap of peaks and genomic elements.pdf")
+print(peak_overlap_per)
+dev.off()
+message("[INFO] The results can be found in the ", outputdir)
 
-assignChromosomeRegion
--- src/bulk/eleAnno.sh
+++ src/bulk/eleAnno.sh
@@ -1,44 +1,60 @@
-eleAnno
+peak_file=$1
+annotation_dir=$2
+outputdir=$3
+# rscript_dir=`dirname "$0"`
+SCRIPT=`echo "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")"`
+rscript_dir=`dirname $SCRIPT`
 
+Help()
+{
+   # Display Help
+   echo "############################################################################" 
+   echo "It is a script for genomic annotation of input peaks."
+   echo
+   echo "Syntax: ./eleAnno.sh peak_file annotation_dir outputdir"
+   echo "options:"
+   echo "peak_file          Peak file with BED format, e.g. cluster1.narrowpeak"
+   echo "annotation_dir     A directory contains predifined genomic element files, e.g. /dir/for/ele"
+   echo "outputdir          The directory for output, e.g. /dir/for/output"
 
-
-
-
-
-
-## import the MACS output
-macs <- system.file("extdata", "MACS_peaks.xls", package="ChIPpeakAnno")
-macsOutput <- toGRanges(macs, format="MACS")
-## annotate the peaks with precompiled ensembl annotation
-data(TSS.human.GRCh38)
-macs.anno <- annotatePeakInBatch(macsOutput, AnnotationData=TSS.human.GRCh38)
-## add gene symbols
-library(org.Hs.eg.db)
-macs.anno <- addGeneIDs(annotatedPeak=macs.anno, 
-                        orgAnn="org.Hs.eg.db", 
-                        IDs2Add="symbol")
-
-if(interactive()){## annotate the peaks with UCSC annotation
-    library(GenomicFeatures)
-    library(TxDb.Hsapiens.UCSC.hg38.knownGene)
-    ucsc.hg38.knownGene <- genes(TxDb.Hsapiens.UCSC.hg38.knownGene)
-    macs.anno <- annotatePeakInBatch(macsOutput, 
-                                     AnnotationData=ucsc.hg38.knownGene)
-    macs.anno <- addGeneIDs(annotatedPeak=macs.anno, 
-                            orgAnn="org.Hs.eg.db", 
-                            feature_id_type="entrez_id",
-                            IDs2Add="symbol")
+   echo "The software BEDTools were needed, if it was not installed, please install it first https://bedtools.readthedocs.io/en/latest/content/installation.html"
+   echo
+   echo "############################################################################" 
 }
+Help
 
 
+echo "-------------------------Begin to work-----------------------------------"
+echo "Begin to work"
+echo "[INFO] Finding the overlap between peaks and genomic elements of 5'UTR, Promoter, Exon, Intron, 3'UTR, intragenic, intergenic, respectively"
 
 
-if(require(TxDb.Hsapiens.UCSC.hg19.knownGene)){
-    aCR<-assignChromosomeRegion(gr1, nucleotideLevel=FALSE, 
-                           precedence=c("Promoters", "immediateDownstream", 
-                                         "fiveUTRs", "threeUTRs", 
-                                         "Exons", "Introns"), 
-                           TxDb=TxDb.Hsapiens.UCSC.hg19.knownGene)
-    barplot(aCR$percentage)
-}
+cd $outputdir
+# elementdir=/gcdata/gcproj/fulong/Data/Genomes/hg38_genomic_element
+ele="utr5 promoter exon intron utr3 intragenic intergenic"
+# peak_file=/gcdata/gcproj/fulong/Data/scCUT_TAG/pipeline_test_k27e3/0708-result/sc_pseudoBulk/group_1/pseudo_bulk_bam/macs2/narrowPeak_fd5_q10_sort_filter_5k2
+for i in $ele
+do
+    echo  [ok] $i
+    bedtools intersect -a $peak_file -b $annotation_dir/${i}.bed -wa > peak_overlap_${i}.bed
+done
 
+rm -rf *.temp.count
+echo [INFO] Counting the peak number..
+wc -l < $peak_file > peak.temp.count
+
+echo [INFO] Counting the overlap..
+
+for i in $ele
+do
+    wc -l < peak_overlap_${i}.bed >> peak_ele_overlap.temp.count
+done
+
+
+Rscript $rscript_dir/eleAnno.r peak_ele_overlap.temp.count peak.temp.count $outputdir 
+
+echo "---------------------------End here--------------------------------------"
+
+# cd /Users/fulongyu/github/CUT-RUNTools-2.0/src/bulk
+# chmod +x *
+# ./eleAnno.sh /Users/fulongyu/github/CUT-RUNTools-2.0/test/FLI1_fimo.bed /Users/fulongyu/github/CUT-RUNTools-2.0/test/hg38_genomic_element /Users/fulongyu/github/CUT-RUNTools-2.0/test
