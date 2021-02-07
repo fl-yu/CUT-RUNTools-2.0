38b4b65609785dbbb470c32997463677ed3328ff
FLYu <57479051+fl-yu@users.noreply.github.com>
2021-02-06 21:28:15 -0500

    Update atactk.install.sh

--- install/atactk.install.sh
+++ install/atactk.install.sh
@@ -18,7 +18,7 @@ then
 patch -p0 -N make_cut_matrix < make_cut_matrix.patch
 fi
 
-cd ~/.local/lib/python2.7/site-packages/atactk
+cd ~/.local/lib/python3.6/site-packages/atactk
 cp $cur/metrics.py.patch .
 patch -p0 -N --dry-run --silent metrics.py < metrics.py.patch 2> /dev/null
 if [ $? -eq 0 ];
