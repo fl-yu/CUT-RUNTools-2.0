98ce3d1c6ab44a2436d7f20b5bb473293794b4e0
fy-yu <yufulong421@gmail.com>
2021-07-23 11:02:14 -0400

    Update get_cuts_single_locus.sh

--- src/bulk/get_cuts_single_locus.sh
+++ src/bulk/get_cuts_single_locus.sh
@@ -38,15 +38,6 @@ eval "$(jq -r '.input_output | to_entries | .[] | .key + "=\"" + .value + "\""' 
 eval "$(jq -r '.motif_finding | to_entries | .[] | .key + "=\"" + .value + "\""' < $configrue_file)"
 chrom_size_file=$SCRIPTPATH/assemblies/chrom."$organism_build"/"$organism_build".chrom.sizes
 
-
-
-bam_file="/broad/sankaranlab/fyu/temp/atac-r4.sorted.bam"
-roi="chr2:60457194-60553654"
-outputdir="/broad/sankaranlab/fyu/temp2"
-samtoolsbin=/broad/software/free/Linux/redhat_7_x86_64/pkgs/samtools/samtools_1.8/bin
-bedtoolsbin=/broad/software/free/Linux/redhat_7_x86_64/pkgs/bedtools/bedtools_2.29.0/bin
-bdg2bwbin=/broad/sankaranlab/fyu/software/CUT-RUNTools-2.0/install
-chrom_size_file=/broad/sankaranlab/fyu/software/CUT-RUNTools-2.0/assemblies/chrom.hg19/hg19.chrom.sizes
 echo "-------------------------Begin to work-----------------------------------"
 echo "Begin to work"
 
@@ -88,42 +79,3 @@ $bdg2bwbin/bedGraphToBigWig $outputdir/"$sample_temp_name".fragment.end.minus.so
 $bdg2bwbin/bedGraphToBigWig $outputdir/"$sample_temp_name".fragment.end.plus.sorted.bdg $chrom_size_file $outputdir/"$sample_name"_plusstrand.bw
 
 rm $outputdir/*"$sample_temp_name"*
-
-
-
-
-head -20 ./newbase.frag.ends.txt | 
-awk -vshift=1 -vOFS="\t" '($6=="+"){ print $1, $2, ($2 + shift), $4, ".", $6}($6=="-"){ print $1, ($3 - shift), $3, $4, ".", $6}' 
-
-awk '{printf "%s\t%d\t%d\t%2.3f\n" , $1,$2,$3,$5}' myBed.bed > myFile.bedgraph
-sort -k1,1 -k2,2n myFile.bedgraph > myFile_sorted.bedgraph
-bedGraphToBigWig myFile_sorted.bedgraph myChrom.sizes myBigWig.bw
-
-
-
-samtools view -F 20 ... : forward strand
-samtools view -f 0x10 ... : reverse strand
-
-bam -> bed -> end.bed -> bedgraph -> bw
-+/-
-
-cd /broad/sankaranlab/fyu/temp
-
-newbam_file=atac-r4.sorted.bam
-samtools view -b $newbam_file | 
-    samtools sort -O bam -n - | bedtools bamtobed -i - -bedpe > ./atac-pe-newbase.frag.ends.txt
-awk -vOFS="\t" '($1==$4 & $2!="-1"){ print $0}' atac-pe-newbase.frag.ends.txt > atac-pe-newbase.frag.ends.paired.txt 
-
-sort -k 1,1 atac-pe-newbase.frag.ends.paired.txt > atac-pe-newbase.frag.ends.paired.sort.txt 
-bedtools genomecov -i atac-pe-newbase.frag.ends.paired.sort.txt -g /broad/sankaranlab/fyu/software/CUT-RUNTools-2.0/assemblies/chrom.hg19/hg19.chrom.sizes -bg  > atac-pe-newbase.frag.ends.paired.sort.bdg
-
-chmod u+x /broad/sankaranlab/fyu/software/CUT-RUNTools-2.0/install/bedGraphToBigWig
-/broad/sankaranlab/fyu/software/CUT-RUNTools-2.0/install/bedGraphToBigWig atac-pe-newbase.frag.ends.paired.sort.bdg /broad/sankaranlab/fyu/software/CUT-RUNTools-2.0/assemblies/chrom.hg19/hg19.chrom.sizes file_name.bigWig
-
-
-bedGraphToBigWig install 
-plus minus 
-please cite
-
-
-
